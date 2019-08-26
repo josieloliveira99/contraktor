1c71b2114ccb6027b707df3ecd3e83e1aee3e5a8
josiel <josielqo@gmail.com>
2019-08-26 07:04:45 -0300

    Modify Models

--- app/Models/Contract.php
+++ app/Models/Contract.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Contract extends Model
 {
-    //
+    protected $fillable = ["title", "start_at", "end_at","pdf_file"];
+
+    public function parties(){
+        return $this->belongsToMany('App\Models\Party')->withTimestamps();
+    }
 }
--- app/Models/Party.php
+++ app/Models/Party.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Party extends Model
 {
-    //
+    protected $fillable = ["cpf", "name", "mail","phone"];
+
+    public function contracts(){
+        return $this->belongsToMany('App\Models\Contract')->withTimestamps();
+    }
 }
